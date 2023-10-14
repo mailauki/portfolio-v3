f9368bc691a391612d7379f03141968b250154b0
Julie Evans <juliemevans17@gmail.com>
2023-10-13 21:15:03 -0500

    Update menu to nav

--- app/components/menu.js
+++ app/components/menu.js
@@ -1,4 +1,7 @@
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+
+import { useState } from 'react'
 
 import { Box, Drawer, List, ListItem, ListItemButton, ListItemText, Toolbar, Typography } from '@mui/material'
 
@@ -7,6 +10,25 @@ import SocialLinks from './social'
 const drawerWidth = '100%';
 
 export default function Menu({ menuOpen, handleClose }) {
+  const pathname = usePathname();
+
+  const pathindex = () => {switch (pathname) {
+    case '/':
+      return 0;
+    case '/about_me':
+      return 1;
+    case '/projects':
+      return 2;
+    default:
+      return 0;
+  }}
+
+  const [selectedIndex, setSelectedIndex] = useState(pathindex);
+
+  const handleListItemClick = (event, index) => {
+    setSelectedIndex(index);
+  };
+
   return (
     <Drawer
       anchor='right'
@@ -16,30 +38,65 @@ export default function Menu({ menuOpen, handleClose }) {
       sx={{
         width: drawerWidth,
         flexShrink: 0,
-        [`& .MuiDrawer-paper`]: { width: drawerWidth, boxSizing: 'border-box' },
+        '.MuiDrawer-paper': { width: drawerWidth, boxSizing: 'border-box' },
       }}
     >
       <Toolbar />
 
-      <List>
-        <ListItem>
-          <ListItemButton component={Link} href="/">
+      <List component='nav'>
+        <ListItem disablePadding>
+          <ListItemButton
+            selected={selectedIndex === 0}
+            onClick={(event) => handleListItemClick(event, 0)}
+            component={Link}
+            href='/'
+            sx={{ textAlign: 'center' }}
+          >
             <ListItemText disableTypography>
-              <Typography variant='h3'>Home</Typography>
+              <Typography
+                variant='h3'
+                color={selectedIndex === 0 ? 'primary' : 'inherit'}
+              >
+                Home
+              </Typography>
             </ListItemText>
           </ListItemButton>
         </ListItem>
-        <ListItem>
-          <ListItemButton component={Link} href="/about_me">
+
+        <ListItem disablePadding>
+          <ListItemButton
+            selected={selectedIndex === 1}
+            onClick={(event) => handleListItemClick(event, 1)}
+            component={Link}
+            href='/about_me'
+            sx={{ textAlign: 'center' }}
+          >
             <ListItemText disableTypography>
-              <Typography variant='h3'>About Me</Typography>
+              <Typography
+                variant='h3'
+                color={selectedIndex === 1 ? 'primary' : 'inherit'}
+              >
+                About Me
+              </Typography>
             </ListItemText>
           </ListItemButton>
         </ListItem>
-        <ListItem>
-          <ListItemButton component={Link} href="/projects">
+        
+        <ListItem disablePadding>
+          <ListItemButton
+            selected={selectedIndex === 2}
+            onClick={(event) => handleListItemClick(event, 2)}
+            component={Link}
+            href='/projects'
+            sx={{ textAlign: 'center' }}
+          >
             <ListItemText disableTypography>
-              <Typography variant='h3'>Projects</Typography>
+              <Typography
+                variant='h3'
+                color={selectedIndex === 2 ? 'primary' : 'inherit'}
+              >
+                Projects
+              </Typography>
             </ListItemText>
           </ListItemButton>
         </ListItem>
